e70e42baea2c8dae52febdc983a675c19465a0a1
Pilseon <psunny0104@gmail.com>
2022-06-16 02:11:05 +0900

    solve: Add solution to BOJ_1546_평균

---
+++ boj/src/BOJ_1546_평균.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BOJ_1546_평균 {
+    public static void main(String[] args) {
+        firstSolution();
+    }
+
+    private static void firstSolution() {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int highest = 0;
+        int avg = 0;
+
+        for (int i = 0; i < N; i++) {
+            int input = sc.nextInt();
+            avg += input;
+            if (input > highest) {
+                highest = input;
+            }
+        }
+
+        System.out.println((100.0 * avg/N) / highest);
+    }
+}
