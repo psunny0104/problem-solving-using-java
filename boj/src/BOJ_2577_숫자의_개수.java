71420248ff98d727ac6c4602a7d1eec861abcb8a
Pilseon <psunny0104@gmail.com>
2022-06-22 01:49:19 +0900

    solve: Add solution to BOJ_2577_숫자의_개수

---
+++ boj/src/BOJ_2577_숫자의_개수.java
@@ -0,0 +1,60 @@
+import java.io.*;
+
+public class BOJ_2577_숫자의_개수 {
+    public static void main(String[] args) throws IOException {
+        // firstSolution();
+        secondSolution();
+    }
+
+    private static void firstSolution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int target = a * b * c;
+        int[] numCount = new int[10];
+        String number = String.valueOf(target);
+
+        for (char num : number.toCharArray()) {
+            numCount[num - '0'] += 1;
+        }
+
+        for (int order : numCount) {
+            sb.append(order).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static void secondSolution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int target = a * b * c;
+        int[] numCount = new int[10];
+
+        while (target > 0) {
+            numCount[target % 10] += 1;
+            target /= 10;
+        }
+
+        for (int order : numCount) {
+            sb.append(order).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
