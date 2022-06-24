46ea8711cb85d86f247b8d53dfa4f1fc0113f07a
Pilseon <psunny0104@gmail.com>
2022-06-24 21:48:58 +0900

    solve: Add solution to BOJ_4344_평균은_넘겠지

---
+++ boj/src/BOJ_4344_평균은_넘겠지.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_4344_평균은_넘겠지 {
+    public static void main(String[] args) throws IOException {
+        firstSolution();
+    }
+
+    private static void firstSolution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int tc = Integer.parseInt(br.readLine());
+        while (tc > 0) {
+            StringTokenizer in = new StringTokenizer(br.readLine());
+            int size = Integer.parseInt(in.nextToken());
+            int[] scores = new int[size];
+            int sum = 0;
+            double avgScore = 0;
+
+            for (int i = 0; i < size; i++) {
+                scores[i] = Integer.parseInt(in.nextToken());
+                sum += scores[i];
+            }
+
+            avgScore = sum / (double) size;
+            int cnt = 0;
+
+            for (int i = 0; i < size; i++) {
+                if (scores[i] > avgScore) {
+                    cnt += 1;
+                }
+            }
+
+            double answer = 100.0 * cnt / size;
+            sb.append(String.format("%.3f", answer)).append("%\n");
+            tc--;
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
