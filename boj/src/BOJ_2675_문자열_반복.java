e3d7763e539ed7b5ad779dd6e4e2b3ca827c01c0
Pilseon <psunny0104@gmail.com>
2022-06-25 23:44:58 +0900

    solve: Add solution to BOJ_2675_문자열_반복

---
+++ boj/src/BOJ_2675_문자열_반복.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_2675_문자열_반복 {
+    public static void main(String[] args) throws IOException {
+        firstSolution();
+    }
+
+    private static void firstSolution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer in = new StringTokenizer(br.readLine());
+
+        int tc = Integer.parseInt(in.nextToken());
+        while (tc > 0) {
+            in = new StringTokenizer(br.readLine());
+            StringBuilder sb = new StringBuilder();
+
+            int cnt = Integer.parseInt(in.nextToken());
+            String target = in.nextToken();
+
+            for (char alphabet : target.toCharArray()) {
+                for (int i = 1; i <= cnt; i++)
+                    sb.append(alphabet);
+            }
+
+            sb.append('\n');
+            bw.write(sb.toString());
+            tc--;
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
