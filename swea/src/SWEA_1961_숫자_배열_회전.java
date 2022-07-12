d0d58f9fe43282451612a92abad7e7ac60ffc9f1
Pilseon <psunny0104@gmail.com>
2022-07-12 23:54:56 +0900

    solve: Add solution to SWEA_1961_숫자_배열_회전

---
+++ swea/src/SWEA_1961_숫자_배열_회전.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class SWEA_1961_숫자_배열_회전 {
+    public static void main(String[] args) throws IOException {
+        firstSolution();
+    }
+
+    private static void firstSolution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer in;
+
+        int tc = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= tc; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] map = new int[n][n];
+
+            for (int i = 0; i < n; i++) {
+                in = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = Integer.parseInt(in.nextToken());
+                }
+            }
+
+            int[][] firstMap = rotate(map, n);
+            int[][] secondMap = rotate(firstMap, n);
+            int[][] thirdMap = rotate(secondMap, n);
+
+            sb.append("#").append(t).append("\n");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    sb.append(firstMap[i][j]);
+                }
+                sb.append(" ");
+                for (int j = 0; j < n; j++) {
+                    sb.append(secondMap[i][j]);
+                }
+                sb.append(" ");
+                for (int j = 0; j < n; j++) {
+                    sb.append(thirdMap[i][j]);
+                }
+                sb.append("\n");
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static int[][] rotate(int[][] map, int size) {
+        int[][] newMap = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                newMap[i][j] = map[size -1 - j][i];
+            }
+        }
+
+        return newMap;
+    }
+}
