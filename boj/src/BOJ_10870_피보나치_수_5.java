692f46fdb16b3765de400aeddd536ed10a2189f0
Pilseon <psunny0104@gmail.com>
2022-08-01 22:12:16 +0900

    solve: Add solution to BOJ_10870_피보나치_수_5

---
+++ boj/src/BOJ_10870_피보나치_수_5.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10870_피보나치_수_5 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int answer = fibo(N);
+        System.out.println(answer);
+    }
+
+    private static int fibo(int n) {
+        if(n == 0)
+            return 0;
+        else if(n == 1)
+            return 1;
+
+        return fibo(n - 2) + fibo(n - 1);
+    }
+}
