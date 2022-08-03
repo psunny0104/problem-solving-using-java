7b55e6695514051bd63a69b9432d401530c8751b
psunny0104 <psunny0104@gmail.com>
2022-08-03 13:20:02 +0900

    solve: Add solution to BOJ_11659_구간_합_구하기_4

--- boj/src/BOJ_11659_구간_합_구하기_4.java
+++ boj/src/BOJ_11659_구간_합_구하기_4.java
@@ -1,42 +1,48 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_11659_구간_합_구하기_4 {
+
     public static void main(String[] args) throws IOException {
-        firstSolution();
-    }
-
-    private static void firstSolution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer input = new StringTokenizer(br.readLine());
-        StringBuilder answer = new StringBuilder();
+        StringTokenizer in = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(input.nextToken());
-        int m = Integer.parseInt(input.nextToken());
+        int size = Integer.parseInt(in.nextToken());
+        int tc = Integer.parseInt(in.nextToken());
+        int[] numbers = new int[size + 1];
+        int[] sumArr = new int[size + 1];
 
-        int[] numbers = new int[n + 1];
-        int[] sumOfNumbers = new int[n + 1];
-        input = new StringTokenizer(br.readLine());
-        numbers[1] = Integer.parseInt(input.nextToken());
-        sumOfNumbers[1] = numbers[1];
-        numbers[0] = sumOfNumbers[0] = 0;
-
-        for (int i = 2; i <= n; i++){
-            numbers[i] = Integer.parseInt(input.nextToken());
-            sumOfNumbers[i] = sumOfNumbers[i - 1] + numbers[i];
+        in = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= size; i++) {
+            numbers[i] = Integer.parseInt(in.nextToken());
         }
 
-        for (int i = 0; i < m; i++) {
-            input = new StringTokenizer(br.readLine());
-            int st = Integer.parseInt(input.nextToken());
-            int ed = Integer.parseInt(input.nextToken());
+        makeSumArr(size, numbers, sumArr);
 
-            answer.append(sumOfNumbers[ed] - sumOfNumbers[st - 1]);
-            answer.append("\n");
+        for(int t = 1; t <= tc; t++) {
+            in = new StringTokenizer(br.readLine());
+            int st = Integer.parseInt(in.nextToken());
+            int ed = Integer.parseInt(in.nextToken());
+
+            int answer = getSum(st, ed, sumArr);
+            sb.append(answer).append("\n");
         }
-
-        bw.write(answer.toString());
-        bw.flush();
+        System.out.println(sb);
     }
+
+    private static void makeSumArr(int size, int[] numbers, int[] sumArr) {
+        sumArr[0] = numbers[0] = 0;
+        for(int i = 1; i <= size; i++ ) {
+            sumArr[i] = sumArr[i - 1] + numbers[i];
+        }
+    }
+
+    private static int getSum(int st, int ed, int[] sumArr) {
+        int diff = sumArr[ed] - sumArr[st - 1];
+        return diff;
+    }
+
 }
