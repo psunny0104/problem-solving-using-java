9ce0143719f88d33dbef905ca0226a6a8b4ea0dd
Pilseon <psunny0104@gmail.com>
2022-08-03 23:29:49 +0900

    solve: Add solution to BOJ_1152_단어의_개수

---
+++ boj/src/BOJ_1152_단어의_개수.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1152_단어의_개수 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer in = new StringTokenizer(br.readLine().trim());
+
+        System.out.println(in.countTokens());
+    }
+}
