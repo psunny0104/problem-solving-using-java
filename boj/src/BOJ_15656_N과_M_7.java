85ffe872dd0966f13336a3fe9ac0951ad757dd28
Pilseon <psunny0104@gmail.com>
2022-08-04 22:29:18 +0900

    solve: Add solution to BOJ_15656_N과_M_7

---
+++ boj/src/BOJ_15656_N과_M_7.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15656_N과_M_7 {
+    // 중복 순열
+    static int N, M;
+    static int[] numbers, inputs;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer in = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(in.nextToken());
+        M = Integer.parseInt(in.nextToken());
+
+        numbers = new int[M];
+        inputs = new int[N];
+
+        in = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            inputs[i] = Integer.parseInt(in.nextToken());
+        }
+
+        Arrays.sort(inputs);
+        perm(0);
+        System.out.println(sb);
+    }
+
+    private static void perm(int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(numbers[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            numbers[cnt] = inputs[i];
+            perm(cnt + 1);
+        }
+    }
+}
