4ddfd5ca6722d8e74ee8109149b3975174c56697
Pilseon <psunny0104@gmail.com>
2022-08-04 22:35:54 +0900

    solve: Add solution to BOJ_15657_N과_M_8

---
+++ boj/src/BOJ_15657_N과_M_8.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_15657_N과_M_8 {
+    // 중복 조합
+    static int N, M;
+    static int[] numbers, inputs;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer in = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(in.nextToken());
+        M = Integer.parseInt(in.nextToken());
+
+        numbers = new int[M];
+        inputs = new int[N];
+
+        in = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            inputs[i] = Integer.parseInt(in.nextToken());
+        }
+
+        Arrays.sort(inputs);
+        comb(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void comb(int cnt, int start) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(numbers[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            numbers[cnt] = inputs[i];
+            comb(cnt + 1, i);
+        }
+    }
+}
