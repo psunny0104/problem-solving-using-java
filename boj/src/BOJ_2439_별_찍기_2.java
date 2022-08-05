58fef58005328302c4726fb81015479da05dbe17
Pilseon <psunny0104@gmail.com>
2022-08-06 01:19:00 +0900

    solve: Add solution to BOJ_2439_별_찍기_2

---
+++ boj/src/BOJ_2439_별_찍기_2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2439_별_찍기_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i ; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
