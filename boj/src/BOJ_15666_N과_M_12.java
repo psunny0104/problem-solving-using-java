47ce98609cf90d5edce4006d8e2751fe1b3f170d
psunny0104 <psunny0104@gmail.com>
2022-08-05 15:17:06 +0900

    solve: Add solution to BOJ_15666_N과_M_12

---
+++ boj/src/BOJ_15666_N과_M_12.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ_15666_N과_M_12 {
+    // 중복 조합 -> 결과 중복 제거
+    static int N, M;
+    static int[] numbers, inputs;
+    static Set<String> linkedHashSet = new LinkedHashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer in = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(in.nextToken());
+        M = Integer.parseInt(in.nextToken());
+
+        numbers = new int[M];
+        inputs = new int[N];
+
+        in = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            inputs[i] = Integer.parseInt(in.nextToken());
+        }
+
+        Arrays.sort(inputs);
+        comb(0, 0);
+        print();
+    }
+
+    private static void print() {
+        StringBuilder sb= new StringBuilder();
+        for (String s : linkedHashSet) {
+            sb.append(s);
+        }
+        System.out.print(sb);
+    }
+
+    private static void comb(int cnt, int start) {
+        StringBuilder sb = new StringBuilder();
+        if (cnt == M) {
+            for (int number : numbers) {
+                sb.append(number).append(" ");
+            }
+            sb.append("\n");
+            linkedHashSet.add(sb.toString());
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            numbers[cnt] = inputs[i];
+            comb(cnt + 1, i);
+        }
+    }
+}
