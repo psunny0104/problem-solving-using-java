f9bd60683df336dfdcf3ce2887a53c0a8d00cb27
Pilseon <psunny0104@gmail.com>
2022-08-08 00:22:48 +0900

    solve: Add solution to BOJ_2018_수들의_합_5

---
+++ boj/src/BOJ_2018_수들의_합_5.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2018_수들의_합_5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int st = 0;
+        int ed = 0;
+        int sum = 0;
+        int answer = 0;
+
+        while (st < N + 1) {
+            if (sum < N) {
+                sum += ed;
+                ed++;
+            } else if (sum > N) {
+                sum -= st;
+                st++;
+            } else if (sum == N) {
+                answer++;
+                sum += ed++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
