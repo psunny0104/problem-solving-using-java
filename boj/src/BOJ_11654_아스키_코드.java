9077764599570ae6af498c9baecdf55c9b5f6288
Pilseon <psunny0104@gmail.com>
2022-08-11 09:42:57 +0900

    solve: Add solution to BOJ_11654_아스키_코드

---
+++ boj/src/BOJ_11654_아스키_코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class BOJ_11654_아스키_코드 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char N = sc.next().charAt(0);
+
+        System.out.println((int)N);
+    }
+}
