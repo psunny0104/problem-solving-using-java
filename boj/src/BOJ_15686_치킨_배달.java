3875775480ff6f83c74cb47f4dbf734b5b2e50e7
Pilseon <psunny0104@gmail.com>
2022-08-12 19:07:47 +0900

    solve: Add solution to BOJ_15686_치킨_배달

--- boj/src/BOJ_15686_치킨_배달.java
+++ boj/src/BOJ_15686_치킨_배달.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class BOJ_15686_치킨_배달 {
 
     static int N, targetStoreCnt, totalStoreCnt ,answer = Integer.MAX_VALUE;
-    static int[][] map;
     static ArrayList<Pair> storeList = new ArrayList<>();
     static ArrayList<Pair> homeList = new ArrayList<>();
     static Pair[] selectedStoreList;
@@ -16,17 +15,15 @@ public class BOJ_15686_치킨_배달 {
 
         N = Integer.parseInt(in.nextToken());
         targetStoreCnt = Integer.parseInt(in.nextToken());
-        map = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             in = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(in.nextToken());
-                if (map[i][j] == 1) {
+                int location = Integer.parseInt(in.nextToken());
+                if (location == 1) {
                     homeList.add(new Pair(i, j));
-                } else if (map[i][j] == 2) {
+                } else if (location == 2) {
                     storeList.add(new Pair(i, j));
-                    map[i][j] = 0;
                 }
             }
         }
@@ -39,30 +36,14 @@ public class BOJ_15686_치킨_배달 {
 
     private static void cmb(int cnt, int start) {
         if (cnt == targetStoreCnt) {
-            // 치킨가게 선택
-            fixStorePosition();
             // 거리 탐색
             calTotalDist();
-            // 맵 복구
-            reStorePosition();
             return;
         }
 
         for (int i = start; i < totalStoreCnt; i++) {
             selectedStoreList[cnt] = storeList.get(i);
             cmb(cnt + 1, i + 1);
-        }
-    }
-
-    private static void fixStorePosition() {
-        for (Pair store : selectedStoreList) {
-            map[store.y][store.x] = 2;
-        }
-    }
-
-    private static void reStorePosition() {
-        for (Pair store : selectedStoreList) {
-            map[store.y][store.x] = 0;
         }
     }
 
