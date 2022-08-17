f2944c5a099494948162844aed4b226e9411041d
Pilseon <psunny0104@gmail.com>
2022-08-17 21:10:36 +0900

    solve: Add solution to SWEA_5644_무선_충전

--- swea/src/SWEA_5644_무선_충전.java
+++ swea/src/SWEA_5644_무선_충전.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class SWEA_5644_무선_충전 {
@@ -47,33 +46,17 @@ public class SWEA_5644_무선_충전 {
                 ));
             }
 
-            // 충전 범위 인지 확인
-                // 중복인지 확인
-                // 중복이면 나누기
-            // 이동
-
-            ArrayList<Integer> aCoveredBCIdx;
-            ArrayList<Integer> bCoveredBCIdx;
+            ArrayList<Integer> aCoveredBCIdx = checkInBCArea(userA);
+            ArrayList<Integer> bCoveredBCIdx = checkInBCArea(userB);
+            charge(aCoveredBCIdx, bCoveredBCIdx);
             for (int idx = 0; idx < time; idx++) {
-                print();
-                System.out.println(idx);
+                move(userA, idx);
+                move(userB, idx);
                 aCoveredBCIdx = checkInBCArea(userA);
                 bCoveredBCIdx = checkInBCArea(userB);
                 charge(aCoveredBCIdx, bCoveredBCIdx);
-                move(userA, idx);
-                move(userB, idx);
-                System.out.println(userA.charge);
-                System.out.println(userB.charge);
-                System.out.println("===============");
             }
 
-            print();
-            aCoveredBCIdx = checkInBCArea(userA);
-            bCoveredBCIdx = checkInBCArea(userB);
-            charge(aCoveredBCIdx, bCoveredBCIdx);
-            System.out.println(userA.charge);
-            System.out.println(userB.charge);
-            System.out.println("===============");
             sb.append("#").append(tc).append(" ").append(userA.charge + userB.charge).append("\n");
         }
         System.out.print(sb);
@@ -82,42 +65,40 @@ public class SWEA_5644_무선_충전 {
     private static void charge(ArrayList<Integer> aCoveredBCIdx, ArrayList<Integer> bCoveredBCIdx) {
         int aSize = aCoveredBCIdx.size();
         int bSize = bCoveredBCIdx.size();
-        if ((aSize == 0 && bSize != 0) || (aSize != 0 && bSize == 0)) {
-            int aGauge = 0;
-            for (Integer coveredBCIdx : aCoveredBCIdx) {
-                aGauge = Math.max(aGauge, bcs.get(coveredBCIdx).power);
-            }
-            userA.charge += aGauge;
-            int bGauge = 0;
-            for (Integer coveredBCIdx : bCoveredBCIdx) {
-                bGauge = Math.max(bGauge, bcs.get(coveredBCIdx).power);
-            }
-            userB.charge += bGauge;
-            System.out.println("a: "+aGauge +" / b: "+bGauge);
-        } else {
-            int aGauge = 0;
-            int bGauge = 0;
-            int tempSumGauge = 0;
-            for (int a = 0; a < aSize; a++) {
-                int aIdx = aCoveredBCIdx.get(a);
-                int tempA = bcs.get(aIdx).power;
-                for (int b = 0; b < bSize; b++) {
-                    int bIdx = bCoveredBCIdx.get(b);
-                    int tempB = bcs.get(bIdx).power;
+        int powerA = 0;
+        int powerB = 0;
+
+        if (aSize != 0 && bSize != 0) {
+            int curPowerSum = 0;
+            for (int b = 0; b < bSize; b++) {
+                int bIdx = bCoveredBCIdx.get(b);
+                for (int a = 0; a < aSize; a++) {
+                    int aIdx = aCoveredBCIdx.get(a);
+
+                    int curPowerA = bcs.get(aIdx).power;
+                    int curPowerB = bcs.get(bIdx).power;
                     if (aIdx == bIdx) {
-                        tempA /= 2;
-                        tempB /= 2;
+                        curPowerA /= 2;
+                        curPowerB /= 2;
                     }
-                    if (tempSumGauge < tempA + tempB) {
-                        aGauge = tempA;
-                        bGauge = tempB;
-                        tempSumGauge = aGauge + bGauge;
+                    if (curPowerSum < curPowerA + curPowerB) {
+                        powerA = curPowerA;
+                        powerB = curPowerB;
+                        curPowerSum = powerA + powerB;
                     }
                 }
             }
-            userA.charge += aGauge;
-            userB.charge += bGauge;
-            System.out.println("a: "+aGauge +" / b: "+bGauge);
+            userA.charge += powerA;
+            userB.charge += powerB;
+        } else {
+            for (Integer coveredBCIdx : aCoveredBCIdx) {
+                powerA = Math.max(powerA, bcs.get(coveredBCIdx).power);
+            }
+            userA.charge += powerA;
+            for (Integer coveredBCIdx : bCoveredBCIdx) {
+                powerB = Math.max(powerB, bcs.get(coveredBCIdx).power);
+            }
+            userB.charge += powerB;
         }
     }
 
@@ -145,7 +126,6 @@ public class SWEA_5644_무선_충전 {
         int c;
         int range;
         int power;
-        ArrayList<User> users = new ArrayList<>();
 
         public BC(int c, int r, int range, int power) {
             this.r = r;
@@ -165,38 +145,5 @@ public class SWEA_5644_무선_충전 {
             this.r = r;
             this.c = c;
         }
-    }
-    static char map[][];
-    private static void print() {
-        map = new char[11][11];
-        for (int i = 1; i <= 10; i++) {
-            Arrays.fill(map[i], '.');
-        };
-
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10 ; j++) {
-                for (int k = 0; k < bcTotalCnt; k++) {
-                    BC bc = bcs.get(k);
-                    int distance = Math.abs(bc.r - i) + Math.abs(bc.c - j);
-                    if (distance <= bc.range) {
-                        int tmp = map[i][j] == '.' ? 0 :  bcs.get(map[i][j] - '0' - 1).power;
-                        if (bcs.get(k).power > tmp)
-                            map[i][j] = (char)(k + 1 + '0');
-                    }
-                }
-            }
-        }
-
-        map[userA.r][userA.c] = 'A';
-        map[userB.r][userB.c] = 'B';
-
-        System.out.println("============================");
-        for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println("============================");
     }
 }
