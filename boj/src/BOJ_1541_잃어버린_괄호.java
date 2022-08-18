6e228e938c671b5e18f39a4fbe71181c6df9bb1a
Pilseon <psunny0104@gmail.com>
2022-08-18 13:47:46 +0900

    solve: Add solution to BOJ_1541_잃어버린_괄호

---
+++ boj/src/BOJ_1541_잃어버린_괄호.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_1541_잃어버린_괄호 {
+
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] minusTokens = br.readLine().split("-");
+
+        int[] subTokens = Arrays.stream(minusTokens[0].split("\\+")).mapToInt(Integer::parseInt).toArray();
+        answer += Arrays.stream(subTokens).sum();
+
+        for (int i = 1; i < minusTokens.length; i++) {
+            subTokens = Arrays.stream(minusTokens[i].split("\\+")).mapToInt(Integer::parseInt).toArray();
+            answer -= Arrays.stream(subTokens).sum();
+        }
+
+        System.out.println(answer);
+    }
+
+}
