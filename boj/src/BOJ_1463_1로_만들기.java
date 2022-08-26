8109df876423b104feffc96247999d05ee0759a6
Pilseon <psunny0104@gmail.com>
2022-08-26 20:25:04 +0900

    solve: Add solution to BOJ_1463_1로_만들기, 15683, 17281, 17471, 17281

---
+++ boj/src/BOJ_1463_1로_만들기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ_1463_1로_만들기 {
+    static int N;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        dp = new int[N*3+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[1] = 0;
+
+        for (int i = 1; i <= N; i++) {
+            dp[i+1] = Math.min(dp[i+1], dp[i] + 1);
+            dp[i*2] = Math.min(dp[i*2], dp[i] + 1);
+            dp[i*3] = Math.min(dp[i*3], dp[i] + 1);
+        }
+
+        System.out.println(dp[N]);
+
+    }
+}
