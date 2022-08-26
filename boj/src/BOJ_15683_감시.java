2d2eb3042a851821295eea487b4da97ed6062b7e
psunny0104 <psunny0104@gmail.com>
2022-08-26 10:27:59 +0900

    solving: Add incomplete solution to BOJ_15683_감시

--- boj/src/BOJ_15683_감시.java
+++ boj/src/BOJ_15683_감시.java
@@ -7,12 +7,11 @@ import java.util.StringTokenizer;
 
 public class BOJ_15683_감시 {
 
-    // 2^ 16
     // 중복순열
     static int R, C, answer = Integer.MAX_VALUE;
     static int[][] ogMap, cpMap;
-    static ArrayList<CCTV> cctvs = new ArrayList<>();
-    static int cctvsSize;
+    static ArrayList<CCTV> cameras = new ArrayList<>();
+    static int cameraCnt;
     static int[] numbers = {0, 1, 2, 3}, selectedDirs;
     static int[] dr = {0, 1, 0, -1};
     static int[] dc = {1, 0, -1, 0};
@@ -32,23 +31,22 @@ public class BOJ_15683_감시 {
             for (int j = 0; j < C; j++) {
                 ogMap[i][j] = Integer.parseInt(st.nextToken());
                 if (ogMap[i][j] > 0 && ogMap[i][j] != 6) {
-                    cctvs.add(new CCTV(i, j, ogMap[i][j]));
+                    cameras.add(new CCTV(i, j, ogMap[i][j]));
                 }
             }
         }
 
-        cctvsSize = cctvs.size();
-
-        selectedDirs = new int[cctvsSize];
+        cameraCnt = cameras.size();
+        selectedDirs = new int[cameraCnt];
 
         search(0);
         System.out.println(answer);
     }
 
     private static void search(int cnt) {
-        if (cnt == cctvsSize) {
-            // 카메라 타입 세팅
-            setCameraType();
+        if (cnt == cameraCnt) {
+            // 카메라 방향 설정
+            setCameraDir();
             // 감시 영역 표현
             markCCTVArea();
             // 사각 지대 확인
@@ -82,63 +80,59 @@ public class BOJ_15683_감시 {
             cpMap[i] = Arrays.copyOf(ogMap[i], ogMap[i].length);
         }
 
-        for (CCTV cctv : cctvs) {
-            switch (cctv.type) {
+        for (CCTV cur : cameras) {
+            switch (cur.type) {
                 case 1:
-                    mark(cctv, cctv.dir);
+                    mark(cur.r, cur.c, cur.dir);
                     break;
                 case 2:
-                    mark(cctv, cctv.dir);
-                    mark(cctv, (cctv.dir + 2) % 4);
+                    mark(cur.r, cur.c, cur.dir);
+                    mark(cur.r, cur.c, (cur.dir + 2) % 4);
                     break;
                 case 3:
-                    mark(cctv, cctv.dir);
-                    mark(cctv, (cctv.dir + 1) % 4);
+                    mark(cur.r, cur.c, cur.dir);
+                    mark(cur.r, cur.c, (cur.dir + 1) % 4);
                     break;
                 case 4:
-                    mark(cctv, cctv.dir);
-                    mark(cctv, (cctv.dir + 1) % 4);
-                    mark(cctv, (cctv.dir + 2) % 4);
+                    mark(cur.r, cur.c, cur.dir);
+                    mark(cur.r, cur.c, (cur.dir + 1) % 4);
+                    mark(cur.r, cur.c, (cur.dir + 2) % 4);
                     break;
                 case 5:
-                    mark(cctv, cctv.dir);
-                    mark(cctv, (cctv.dir + 1) % 4);
-                    mark(cctv, (cctv.dir + 2) % 4);
-                    mark(cctv, (cctv.dir + 3) % 4);
+                    mark(cur.r, cur.c, cur.dir);
+                    mark(cur.r, cur.c, (cur.dir + 1) % 4);
+                    mark(cur.r, cur.c, (cur.dir + 2) % 4);
+                    mark(cur.r, cur.c, (cur.dir + 3) % 4);
                     break;
             }
         }
     }
 
-    private static void mark(CCTV cur, int dir) { // 동, 남, 서, 북
-        int nr = cur.r;
-        int nc = cur.c;
+    private static void mark(int r, int c, int dir) { // 동, 남, 서, 북
+        int nr = r;
+        int nc = c;
 
-        int limit = Math.max(R, C);
-        for (int k = 0; k < limit; k++) {
+        while (true) {
             nr += dr[dir];
             nc += dc[dir];
 
             if (nr < 0 || nc < 0 || nr >= R || nc >= C) {
-                continue;
+                break;
             }
 
             if (cpMap[nr][nc] == 6) {
                 break;
             }
 
-            if (cpMap[nr][nc] == 0)
-                cpMap[nr][nc] = -1;
+            cpMap[nr][nc] = -1;
         }
 
     }
 
-    private static void setCameraType() {
-        for (int i = 0; i < cctvsSize; i++) {
-            cctvs.get(i).dir = selectedDirs[i];
-            if (cctvs.get(i).type == 2) {
-                cctvs.get(i).dir %= 2;
-            }
+    private static void setCameraDir() {
+        for (int i = 0; i < cameraCnt; i++) {
+            CCTV cur = cameras.get(i);
+            cur.dir = selectedDirs[i];
         }
     }
 
