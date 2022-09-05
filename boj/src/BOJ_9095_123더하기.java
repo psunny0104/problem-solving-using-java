b9a928cbc6e30566837bfca3015b253f62b2bd4f
psunny0104 <psunny0104@gmail.com>
2022-09-05 18:34:46 +0900

    solve: Add solution to BOJ_9095_123더하기

--- boj/src/BOJ_9095_123더하기.java
+++ boj/src/BOJ_9095_123더하기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class BOJ_9095_123더하기 {
 
@@ -14,17 +13,15 @@ public class BOJ_9095_123더하기 {
         T = Integer.parseInt(br.readLine());
 
         dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
         while (T-- > 0) {
             N = Integer.parseInt(br.readLine());
-            Arrays.fill(dp, 0);
-            dp[1] = 1;
-            dp[2] = 2;
-            dp[3] = 4;
-
-            for (int i = 4; i <= N; i++) {
-                dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
-            }
-
             sb.append(dp[N]).append("\n");
         }
         System.out.print(sb);
