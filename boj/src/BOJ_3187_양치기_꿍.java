3eaee83e408f2e4baed8e15b5e518c55e68485d7
psunny0104 <psunny0104@gmail.com>
2022-10-21 03:20:00 +0900

    solve: Add solution to BOJ_3187_양치기_꿍

---
+++ boj/src/BOJ_3187_양치기_꿍.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_3187_양치기_꿍 {
+
+    static int R, C, sheep, wolf;
+    static char[][] map;
+    static boolean[][] isVisited;
+    static int[] dr = {0, 0, 1, -1};
+    static int[] dc = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        map = new char[R][C];
+        isVisited = new boolean[R][C];
+        for (int i = 0; i < R; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (isVisited[i][j]) {
+                    continue;
+                }
+                if (map[i][j] != '#') {
+                    bfs(i, j);
+                }
+            }
+        }
+
+        System.out.println(sheep + " " + wolf);
+    }
+
+    private static void bfs(int r, int c) {
+        Queue<Point> q = new ArrayDeque<>();
+        q.offer(new Point(r, c));
+        isVisited[r][c] = true;
+
+        int curSheep = 0;
+        int curWolf = 0;
+
+        while (!q.isEmpty()) {
+            Point cur = q.poll();
+            int cr = cur.r;
+            int cc = cur.c;
+
+            if (map[cr][cc] == 'v') {
+                curWolf++;
+            } else if (map[cr][cc] == 'k') {
+                curSheep++;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nr = cr + dr[d];
+                int nc = cc + dc[d];
+
+                if (nr < 0 || nc < 0 || nr >= R || nc >= C) {
+                    continue;
+                }
+
+                if (isVisited[nr][nc]) {
+                    continue;
+                }
+
+                if (map[nr][nc] == '#') {
+                    continue;
+                }
+
+                q.offer(new Point(nr, nc));
+                isVisited[nr][nc] = true;
+
+            }
+        }
+
+        if (curSheep > curWolf) {
+            sheep += curSheep;
+        } else {
+            wolf += curWolf;
+        }
+    }
+
+    private static class Point {
+
+        int r, c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+}
